8401abfd5c4bb6eec69bb70d3f77289da7e72697
Hung Tran <tmhung201088@gmail.com>
2019-12-10 16:36:06 +0700

    Draw a rectangle

--- web-app-parcel/index.ts
+++ web-app-parcel/index.ts
@@ -1,2 +1,23 @@
-import message from './message'
-console.log(message)
+class DrawingApp {
+    private canvas: HTMLCanvasElement;
+    private ctx: CanvasRenderingContext2D;
+    constructor(canvasId: string) {
+        let canvas = document.getElementById(canvasId) as
+            HTMLCanvasElement;
+        let context = canvas.getContext("2d");
+
+        this.canvas = canvas;
+        this.ctx = context;
+
+        this.redraw();
+
+    }
+
+    private redraw() {
+        const ctx = this.ctx;
+        ctx.fillStyle = 'black';
+        ctx.fillRect(30, 30, 150, 100);
+    }
+}
+
+new DrawingApp('canvas');
